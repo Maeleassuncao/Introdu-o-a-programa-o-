e6b0b3af12a3bcf745d430b8320e20d363ee7b80
Maeleassuncao <161831704+Maeleassuncao@users.noreply.github.com>
2024-07-12 08:51:06 -0300

    array

---
+++ 01.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<clocale>
+using namespace std;
+int main (){
+	int idade[]={20,21,22,23,24};
+	
+for(int m=0;m<5;m++){
+	cout<<m<<" "<<idade[m]<<endl;
+}	
+return 0;
+	
+}
