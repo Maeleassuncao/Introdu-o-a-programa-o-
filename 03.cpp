c147ead52b7867bdd8d90ee228a25f7dbe78e271
Maeleassuncao <161831704+Maeleassuncao@users.noreply.github.com>
2024-07-12 09:04:20 -0300

    Add files via upload

---
+++ 03.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<clocale>
+using namespace std;
+int main (){
+	int dados[9];
+	for(int i=9;i>0;i--){
+		dados[i]=i-1;
+cout<<i<<" "<<dados[i]<<endl;
+	
+	}
+	return 0;
+	}
